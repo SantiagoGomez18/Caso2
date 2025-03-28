3779f7f0838880e53a1aba71a294ab7a740ac058
Santiago Gomez <S.gomezo2@uniandes.edu.co>
2025-03-27 20:30:08 -0500

    Xd

---
+++ src/Opcion1/GeneradorReferencias.java
@@ -0,0 +1,15 @@
+package Opcion1;
+import Opcion1.Imagen;
+import Opcion1.FiltroSobel;
+
+public class GeneradorReferencias {
+    Imagen imagen;
+    FiltroSobel filtroSobel;
+
+    public GeneradorReferencias(Imagen imagen) {
+        this.imagen = imagen;
+        
+    }
+
+
+}
